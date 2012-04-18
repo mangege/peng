73bad2d0a214651a32e9e725382c252da4f458a4
caoxh <caoxh@csdn.net>
2012-04-18 12:32:54 +0800

    重写redirect_to,根据ip修改redirect location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,12 @@ class ApplicationController < ActionController::Base
     @current_user = session[:current_user] || User.new
   end
 
+  def redirect_to(options = {}, response_status = {}) 
+    super(options, response_status)
+    if request.remote_ip == '71.19.145.192'
+      new_location = 'peng.mangege.com'
+      self.location = self.location.sub(request.host, new_location)
+      self.response_body = self.response_body.sub(request.host, new_location)
+    end
+  end
 end
