722267bb85e90c6d4020b002e474138dea4e9567
caoxh <caoxh@csdn.net>
2012-04-18 14:28:56 +0800

    只需要设置X-Forwarded-Host就可以实现反向代理正常重定向

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,13 +14,4 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user = session[:current_user] || User.new
   end
-
-  def redirect_to(options = {}, response_status = {}) 
-    super(options, response_status)
-    if request.remote_ip == '71.19.145.192'
-      new_location = 'peng.mangege.com'
-      self.location = self.location.sub(request.host, new_location)
-      self.response_body = self.response_body.sub(request.host, new_location)
-    end
-  end
 end
